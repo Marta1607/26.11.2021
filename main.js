0bd6d8a20ce30001e70c3b61fd6f7340aecd8483
Marta1607 <marta.kar.zuber@gmail.com>
2021-11-26 14:02:30 +0100

    2

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ function pobieranie() {
 
 function jakosc() {
     const dobre = document.getElementById('inp').value
-    if (dobre.length > 6) {
+    //sprawdza czy dany element spełnia wymagania odnośnie długości
+    if (dobre.length > 6 && dobre.search(/[0-9]/)!=-1) {
+        //daje napis gdy po kliknięciu przycisku warunek spełnia/niespełnia odpowiednich wymagań
         document.getElementById('p1').innerHTML = 'odpowienie hasło'
     }
-    if (dobre.length > 4 && dobre.length < 7){
-        document.getElementById('p1').innerHTML = 'średnie hasło'
+    if (dobre.length > 3 && dobre.length < 7 && dobre.search(/[0-9]/)!=-1){
+        document.getElementById('p1').innerHTML = 'średnie hasło' 
     }
     if (dobre.length < 4){
         document.getElementById('p1').innerHTML = 'słabe hasło'
     }
-        console.log(dobre)
+        
 }
